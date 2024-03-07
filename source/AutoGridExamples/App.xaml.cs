2442953d2af9bbbec2b48068cca257ac69c5f515
LaughingLeader <laughingleader@gmail.com>
2024-03-06 21:26:51 -0600

    Upgrade to Net 8

--- source/AutoGridExamples/App.xaml.cs
+++ source/AutoGridExamples/App.xaml.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace AutoGridExamples
 {
-    public partial class App : Application
-    {
-        //protected override void OnStartup(StartupEventArgs e)
-        //{
-        //    base.OnStartup(e);
-        //    if (true)
-        //    {
-        //        this.Shutdown();
-        //    }
-        //}
-    }
+	public partial class App : Application
+	{
+		//protected override void OnStartup(StartupEventArgs e)
+		//{
+		//    base.OnStartup(e);
+		//    if (true)
+		//    {
+		//        this.Shutdown();
+		//    }
+		//}
+	}
 }
